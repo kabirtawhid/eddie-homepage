baef863b2ea908dd5109808a443c353352bec1b6
Kabir Tawhid <kabirfromtawhid@gmail.com>
2023-09-12 23:16:34 +0600

    Completed project section for desktop 1440px screen

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -4,24 +4,30 @@ function Projects({ data }) {
       <h3 className="heading-2 m-m-2 m-d-2">
         Good design means <br /> good business
       </h3>
-      <div className="mt-12 flex flex-col gap-9 m-m-1">
+      <div className="mt-12 grid grid-cols-1 2xl:grid-cols-2  gap-9 md:gap-5  m-m-1 m-d-2">
         {data?.map((x) => (
-          <div key={x.name}>
+          <div
+            className={`h-[auto] 2xl:h-[800px] flex flex-col ${
+              data.indexOf(x) % 2 === 1 ? "justify-start" : "justify-end"
+            } `}
+            key={x.name}
+          >
             <img
               className="w-full rounded-[24px]"
               src={`/src/assets/${x.photo}`}
               alt=""
             />
-            <h5 className="mt-[13px] text-gray-3 text-[12px] font-light font-poppins ">
+            <h5 className="mt-[13px] text-gray-3 text-[12px] 2xl:text-[14px] font-light font-poppins ">
               {x.category}
             </h5>
-            <h4 className="text-gray-1 text-lg font-poppins font-medium">
+            <h4 className="text-gray-1 text-lg 2xl:text-2xl font-poppins font-medium">
               {x.name}
             </h4>
           </div>
         ))}
-
-        <button className="flex items-center gap-2 text-lg text-blue-1 font-medium ">
+      </div>
+      <div className="m-m-1 m-d-2 mt-9 relative">
+        <button className="flex items-center gap-2 text-lg text-blue-1 font-medium absolute right-0 top-[-80px]">
           <p className="pb-1">see more</p>
           <span className="material-icons">arrow_right_alt</span>
         </button>
