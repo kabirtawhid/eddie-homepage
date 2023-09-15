b33adbc13fd2f970262ef4a60253c9e1240099ed
Kabir Tawhid <kabirfromtawhid@gmail.com>
2023-09-15 13:33:36 +0600

    Completed Testmonial section for desktop 1440 screen

--- src/Testmonial.tsx
+++ src/Testmonial.tsx
@@ -1,12 +1,15 @@
 function Testmonial({ data }) {
   return (
-    <div className="mt-[100px] m-m-1" id="testmonial">
+    <div
+      className="mt-[100px] 2xl:mt-[196px] m-m-1 m-d-2 max-w-[920px]"
+      id="testmonial"
+    >
       {data?.map((x) => (
         <div key={x.author}>
-          <p className="text-gray-1 font-poppins text-2xl leading-9  font-medium">
+          <p className="text-gray-1 font-poppins text-2xl 2xl:text-[36px] leading-9 2xl: leading-[54px]  font-medium">
             {x.description}
           </p>
-          <div className="flex gap-[30px] mt-[27px]">
+          <div className="flex gap-[30px] mt-[27px] 2xl:mt-[45px]">
             <img
               className="w-[82px] h-[82px] rounded-[12px]"
               src={`/src/assets/${x.photo}`}
